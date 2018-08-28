4beee86f5d34bd7ef9edf6dfdf321133c6329c0a
defaultdance12 <42258296+defaultdance12@users.noreply.github.com>
2018-08-27 22:05:23 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("xQc", {type: "WATCHING"});
+  bot.user.setActivity("godfather", {type: "Groping"});
 
 });
 
